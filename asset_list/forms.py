844738d5a461cd9912269ab9f3d2f4c03d646d76
Goto Norio <gotonorio@gmail.com>
2021-04-20 10:24:37 +0900

    asset_list/forms.pyの見直し

--- asset_list/forms.py
+++ asset_list/forms.py
@@ -4,26 +4,23 @@ from asset_list.models import AssetList, Master_assetlist
 
 
 class AssetListForm(forms.ModelForm):
-    """ 資産データ追加用のFormを宣言
-        https://qiita.com/felyce/items/5042db0792c9f7d01c1e
-    """
+    """ 資産データ用のForm """
+    # 本来はview関数で初期値を設定すべきか?
     max_ki = AssetList.objects.aggregate(ki=Max('ki'))
     if max_ki["ki"] is None:
         ki = forms.IntegerField(label='期', initial=0)
     else:
         ki = forms.IntegerField(label='期', initial=max_ki["ki"]+1)
-    # 以下はtemplatesでformフィールド名を表示させるため。無くても表示がmodelsの要素名になるだけ。
-    master = forms.ModelChoiceField(
-        queryset=Master_assetlist.objects.filter(alive=True).order_by('sequense'),
-        label='資産種別'
-    )
-    asset = forms.IntegerField(label='資産額')
 
-    # インナークラスでmodel、fieldを指定する。（これでモデルに紐づける）
-    # （https://djangogirlsjapan.gitbooks.io/workshop_tutorialjp/content/django_forms/）
     class Meta:
         model = AssetList
         fields = ('ki', 'account_type', 'master', 'asset', 'comment',)
+        labels = {
+            'account_type': '会計区分',
+            'master': '資産種別',
+            'asset': '資産額',
+            'comment': '説明',
+        }
 
     # fieldにbootstrap用のclassを設定する。
     def __init__(self, *args, **kwargs):
@@ -38,16 +35,23 @@ class AssetListForm(forms.ModelForm):
 
 class Master_assetForm(forms.ModelForm):
     """ 資産マスターの登録用フォーム """
-    # フィールド名を設定するためにlabelを追加。
-    name = forms.CharField(label='口座名', max_length=128)
+    name = forms.CharField(label='口座名', max_length=64)
     account_number = forms.CharField(label='口座番号', max_length=24, initial='-')
     sequense = forms.IntegerField(label='表示順序')
     isAsset = forms.IntegerField(label='資産(1)/負債(0)', initial=1)
-    alive = forms.BooleanField(label='有効', required=False)
+    alive = forms.NullBooleanField(label='有効', initial=True)
 
     class Meta:
         model = Master_assetlist
         fields = ("sequense", "name", "account_number", "isAsset", "alive", "comment")
+        labels = {
+            'comment': '備考',
+        }
+        widgets = {
+            'comment': forms.Textarea(attrs={
+                'rows': '3',
+            })
+        }
 
     # fieldにclassを一括設定する。
     def __init__(self, *args, **kwargs):
@@ -56,10 +60,8 @@ class Master_assetForm(forms.ModelForm):
         self.fields['account_number'].widget.attrs["class"] = "input"
         self.fields['sequense'].widget.attrs["class"] = "input"
         self.fields['isAsset'].widget.attrs["class"] = "input"
-        self.fields['alive'].widget.attrs["class"] = "checkbox"
+        self.fields['alive'].widget.attrs["class"] = "select"
         self.fields['comment'].widget.attrs["class"] = "textarea"
-        # for field in self.fields.values():
-        #     field.widget.attrs["class"] = "input is-size-6"
 
 
 class BalanceSheetForm(forms.Form):
